f025fe6d41afd561131cc41364e1940066e69894
jernejk <jernej.kavka@windowslive.com>
2015-07-12 13:57:30 +0200

    UWP implementation with custom calculations.

--- BatteryDemo.Universal/MainPage.xaml.cs
+++ BatteryDemo.Universal/MainPage.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using BatteryDemo.Universal.Utils;
+using System;
+using Windows.Devices.Power;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace BatteryDemo.Universal
 {
     /// <summary>
@@ -24,7 +13,58 @@ namespace BatteryDemo.Universal
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            UpdateBatteryStatus();
+            Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
+        }
+
+        private async void AggregateBattery_ReportUpdated(Battery sender, object args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, UpdateBatteryStatus);
+        }
+
+        private void UpdateBatteryStatus()
+        {
+            BatteryReport report = Battery.AggregateBattery.GetReport();
+
+            // Built-in values
+            DesignedMaxCapacity.Text = $"Designed max capacity: {report.DesignCapacityInMilliwattHours?.ToString() ?? "NaN"} mWh";
+            CurrentCapacity.Text = $"Current/full capacity: {report.RemainingCapacityInMilliwattHours?.ToString() ?? "NaN"}/{report.FullChargeCapacityInMilliwattHours?.ToString() ?? "NaN"} mWh";
+            BatteryState.Text = $"Battery status: {report.Status}";
+            ChargeRate.Text = $"Charge rate: {report.ChargeRateInMilliwatts?.ToString() ?? "NaN"} mWh";
+            
+            // Calculated values (Utils/BatteryReportExtensions.cs)
+            BatteryPercentage.Text = $"Battery life in percents: {report.BatteryLevelInPercentage()?.ToString() ?? "--"} %";
+
+            if (report.IsChargingBasedOnConsumption())
+            {
+                string timeText = GetTimeAsString(report.EstimateTimeToCharge());
+                TimeToCharge.Text = $"Time to charge: {timeText}";
+
+                timeText = GetTimeAsString(report.EstimateTimeToChargeFromZeroToFull());
+                TimeToChargeFromZeroToFull.Text = $"Charge from zero to full: {timeText}";
+            }
+            else if (report.IsDischargingBasedOnConsumption())
+            {
+                string timeText = GetTimeAsString(report.EstimateTimeToDischarge());
+                TimeToCharge.Text = $"Time to discharge: {timeText}";
+
+                timeText = GetTimeAsString(report.EstimateTimeToDischargeFromFullToZero());
+                TimeToChargeFromZeroToFull.Text = $"Discharge from full to zero: {timeText}";
+            }
+            else
+            {
+                TimeToCharge.Text = "Time to charge: --:--";
+                TimeToChargeFromZeroToFull.Text = "Charge from zero to full: --:--";
+            }
+
+            BatteryHealth.Text = $"Battery health: {report.BatteryHealthInPercentage()?.ToString() ?? "--"} %";
+        }
+
+        private static string GetTimeAsString(TimeSpan? time)
+        {
+            return time.HasValue ? time.Value.ToString("hh\\:mm\\:ss") : "--:--";
         }
     }
 }
---
+++ BatteryDemo.Universal/Utils/BatteryReportExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Windows.Devices.Power;
+
+namespace BatteryDemo.Universal.Utils
+{
+    public static class BatteryReportExtensions
+    {
+        public static bool IsBatteryPresent(this BatteryReport report)
+            => report.Status != Windows.System.Power.BatteryStatus.NotPresent;
+
+        public static bool IsChargingBasedOnConsumption(this BatteryReport report) =>
+            report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value > 0;
+
+        public static bool IsDischargingBasedOnConsumption(this BatteryReport report) =>
+            report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value < 0;
+
+        public static double? BatteryLevel(this BatteryReport report)
+        {
+            if (report.RemainingCapacityInMilliwattHours.HasValue && report.FullChargeCapacityInMilliwattHours.HasValue)
+            {
+                int remainingCapacity = report.RemainingCapacityInMilliwattHours.Value;
+                int fullCapacity = report.FullChargeCapacityInMilliwattHours.Value;
+                return remainingCapacity / (double)fullCapacity;
+            }
+
+            // Not enough data available
+            return null;
+        }
+
+        public static double? BatteryLevelInPercentage(this BatteryReport report)
+        {
+            double? batteryLevel = BatteryLevel(report);
+            return batteryLevel.HasValue ? batteryLevel.Value * 100d : (double?)null;
+        }
+
+        public static double? BatteryHealthInPercentage(this BatteryReport report)
+        {
+            if (report.FullChargeCapacityInMilliwattHours.HasValue && report.DesignCapacityInMilliwattHours.HasValue)
+            {
+                return (report.FullChargeCapacityInMilliwattHours.Value / (double)report.DesignCapacityInMilliwattHours.Value) * 100d;
+            }
+
+            // Not enough data
+            return null;
+        }
+
+        public static TimeSpan? EstimateTimeToCharge(this BatteryReport report)
+        {
+            // Ignore battery status because we can have charge data even if status is not charging (rare but possible)
+            // Also check if full and remaining battery capacity is available.
+            if (report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value > 0 &&
+                report.FullChargeCapacityInMilliwattHours.HasValue && report.RemainingCapacityInMilliwattHours.HasValue)
+            {
+                int remainingCapacityToCharge = report.FullChargeCapacityInMilliwattHours.Value - report.RemainingCapacityInMilliwattHours.Value;
+                double hoursToCharge = remainingCapacityToCharge / (double)report.ChargeRateInMilliwatts.Value;
+
+                return TimeSpan.FromHours(hoursToCharge);
+            }
+
+            // Not enough data or valid data
+            return null;
+        }
+
+        public static TimeSpan? EstimateTimeToChargeFromZeroToFull(this BatteryReport report)
+        {
+            // Ignore battery status because we can have charge data even if status is not charging (rare but possible)
+            if (report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value > 0 &&
+                report.FullChargeCapacityInMilliwattHours.HasValue)
+            {
+                double hoursToCharge = CalculateHoursLeft(report, report.FullChargeCapacityInMilliwattHours.Value);
+                return TimeSpan.FromHours(hoursToCharge);
+            }
+
+            // Not enough data or valid data
+            return null;
+        }
+
+        public static TimeSpan? EstimateTimeToDischarge(this BatteryReport report)
+        {
+            // Ignore battery status because battery consumption can be higher than charging. (playing high-demand games or using navigation in a car with a weak charger or recording videos while charging battery)
+            // Also check if full battery capacity is available.
+            if (report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value < 0 &&
+                report.RemainingCapacityInMilliwattHours.HasValue)
+            {
+                double hoursToDischarge = CalculateHoursLeft(report, report.RemainingCapacityInMilliwattHours.Value);
+                return TimeSpan.FromHours(-hoursToDischarge);
+            }
+
+            // Not enough data or valid data
+            return null;
+        }
+
+        public static TimeSpan? EstimateTimeToDischargeFromFullToZero(this BatteryReport report)
+        {
+            // Ignore battery status because battery consumption can be higher than charging. (playing high-demand games or using navigation in a car with a weak charger or recording videos while charging battery)
+            // Also check if full battery capacity is available.
+            if (report.ChargeRateInMilliwatts.HasValue && report.ChargeRateInMilliwatts.Value < 0 &&
+                report.RemainingCapacityInMilliwattHours.HasValue)
+            {
+                double hoursToDischarge = CalculateHoursLeft(report, report.FullChargeCapacityInMilliwattHours.Value);
+                return TimeSpan.FromHours(-hoursToDischarge);
+            }
+
+            // Not enough data or valid data
+            return null;
+        }
+
+        /// <summary>
+        /// Calculate in how long will it take to charge or discharge a battery with set capacity and current charge rate.
+        /// NOTE: Charge is negative when discharging!
+        /// </summary>
+        /// <param name="report">Battery report</param>
+        /// <param name="capacityInMiliwattHours">Capacity used for calculating charge/discharge time</param>
+        /// <returns>Returns how long it will take to charge or discharge in hours. Positive number is charging and negative is discharging.</returns>
+        private static double CalculateHoursLeft(BatteryReport report, double capacityInMiliwattHours)
+            => capacityInMiliwattHours / report.ChargeRateInMilliwatts.Value;
+    }
+}
