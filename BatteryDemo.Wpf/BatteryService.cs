1922dda68d1359daf6f654d92cfde230269c076f
jernejk <jernej.kavka@windowslive.com>
2015-07-12 19:01:56 +0200

    Added WPF implementation for getting battery data.

---
+++ BatteryDemo.Wpf/BatteryService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BatteryDemo.Wpf
+{
+    public class BatteryService
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int GetSystemPowerStatus(ref SYSTEM_POWER_STATUS lpSystemPowerStatus);
+
+        public struct SYSTEM_POWER_STATUS
+        {
+            public ACLineStatus ACLineStatus;
+            public BatteryFlag BatteryFlag;
+            public byte BatteryLifePercent;
+            public byte Reserved1;
+            public int BatteryLifeTime;
+            public int BatteryFullLifeTime;
+
+            public TimeSpan CalculatedBatteryLifeTime { get { return TimeSpan.FromSeconds(BatteryLifeTime); } }
+
+            public TimeSpan CalculatedBatteryFullLifeTime { get { return TimeSpan.FromSeconds(BatteryFullLifeTime); } }
+
+            public bool IsCharging { get { return (BatteryFlag & BatteryFlag.Charging) > 0; } }
+        }
+
+        [Flags]
+        public enum BatteryFlag : byte
+        {
+            High = 1,
+            Low = 2,
+            Critical = 4,
+            Charging = 8,
+            NoSystemBattery = 128,
+            Unknown = 255
+        }
+
+        public enum ACLineStatus : byte
+        {
+            Offline = 0,
+            Online = 1,
+            Unknown = 255
+        }
+
+        public static SYSTEM_POWER_STATUS GetStatus()
+        {
+            SYSTEM_POWER_STATUS SPS = new SYSTEM_POWER_STATUS();
+            GetSystemPowerStatus(ref SPS);
+            return SPS;
+        }
+    }
+}
---
+++ BatteryDemo.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BatteryDemo.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            UpdateBattery();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateBattery();
+        }
+
+        private void UpdateBattery()
+        {
+            BatteryService.SYSTEM_POWER_STATUS status = BatteryService.GetStatus();
+            BatteryLifePercent.Text = string.Format("{0} %", status.BatteryLifePercent);
+            BatteryFlag.Text = string.Format("Battery flag: {0}", status.BatteryFlag);
+            ACLineStatus.Text = string.Format("AC line status: {0}", status.ACLineStatus);
+            BatteryLifeTime.Text = string.Format("Battery life time: {1} ({0} sec)", status.BatteryLifeTime, status.CalculatedBatteryLifeTime);
+            BatteryFullLifeTime.Text = string.Format("Battery full life time: {1} ({0} sec)", status.BatteryFullLifeTime, status.CalculatedBatteryFullLifeTime);
+            IsCharging.Text = string.Format("Is charging: {0}", status.IsCharging);
+        }
+    }
+}
